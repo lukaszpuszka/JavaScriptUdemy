025b1843909952898596e7d5eb40fbe4d60fa357
Lukasz <49566372+lukaszpuszka@users.noreply.github.com>
2023-02-02 00:12:27 +0300

    obiekt

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -2,8 +2,24 @@
 
 // "Jonas has 3 friends, and his best friend is called michael"
 
-const jonas = ["Michael", "Bob", "Andrew"];
+// const jonas = ["Michael", "Bob", "Andrew"];
 
-console.log(
-  `Jonas has ${jonas.length} friends, and his best friend is called ${jonas[0]}`
-);
+// console.log(
+//   `Jonas has ${jonas.length} friends, and his best friend is called ${jonas[0]}`
+// );
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmiddenten",
+  job: "teacher",
+  dateOfBirth: 1991,
+  hasDriversLincense: false,
+
+  getSummary: function () {
+    return `${this.firstName} is a 46 year old ${this.job}, and ${
+      this.hasDriversLincense ? "has" : "does not"
+    } has a driver's lincese`;
+  },
+};
+
+console.log(jonas.getSummary());
